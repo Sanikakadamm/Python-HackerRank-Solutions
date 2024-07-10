78bf10860e78f2ccd070471939ae2527481c0c2a
Sanika kadam <96229011+Sanikakadamm@users.noreply.github.com>
2024-07-10 19:38:14 +0530

    IterTools_Product.py

---
+++ IterTools_Product.py
@@ -0,0 +1,7 @@
+from itertools import product
+
+A = list(map(int,input().strip().split()))
+B = list(map(int,input().strip().split()))
+
+ans = [str(i) for i in list(product(A,B))]
+print (' '.join(ans))
